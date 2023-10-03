36f1f962cfa8b49c12c897327fc4a7cf821dd6a1
unknown <azb1378@gmail.com>
2023-10-03 14:51:25 +0330

    last-update

--- load_saved_model.py
+++ load_saved_model.py
@@ -4,16 +4,26 @@ import numpy as np
 import tensorflow as tf
 import os
 
-IMAGE_SHAPE = (224,224)
+IMAGE_SHAPE = (256,256)
 tumors = ["glioma", "meningioma", "notumor", "pituitary"]
 
 class load_model:
     def __init__(self):
-        self.save_model = keras.models.load_model(os.path.join("trained_models", "trained_on_all_datasets_94.h5"))
+        self.save_model = tf.keras.models.load_model(os.path.join("trained_models", "ensemble_model_v2.h5"))
 
     def predict(self, image):
+        image = np.array(image)
+        if len(image.shape) > 2 :
+            image = tf.image.resize(image, IMAGE_SHAPE)
+        else:
+            image_rgb = np.empty((image.shape[0], image.shape[1], 3), dtype=image.dtype)
+            image_rgb[:, :, 0] = image
+            image_rgb[:, :, 1] = image
+            image_rgb[:, :, 2] = image
+            image = image_rgb
+            image = tf.image.resize(np.array(image), IMAGE_SHAPE)[:, :, :3]
 
-        image = tf.image.resize(np.array(image)[:,:,:3], IMAGE_SHAPE)
+        
         image = np.array([image])
 
         prediction_value = np.argmax(self.save_model.predict(image))
